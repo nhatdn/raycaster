a6fccd173232a96f71a8674d747fe9407dc93a8c
Nguyễn Đình Nhật <85542339+nhatdn@users.noreply.github.com>
2024-08-03 15:16:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ scene.add(hemisphereLight);
 const AmbientLight = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(AmbientLight);
 
+// floor
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(500, 500), new THREE.MeshPhongMaterial({ color: 'gray' }));
 plane.rotation.x = - Math.PI / 2;
 plane.castShadow = true;
@@ -32,18 +33,25 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 const loader = new GLTFLoader();
 
+// group house will save all mesh in home.glb
+const house = new THREE.Group();
+scene.add(house);
+
 loader.load('./home.glb', function (gltf) {
     const model = gltf.scene;
     window.model = model;
-    for(let i = 0; i < model.children.length; i++) {
+    for (let i = 0; i < model.children.length; i++) {
         model.children[i].castShadow = true;
         model.children[i].receiveShadow = true;
         model.children[i].name = i.toString();
-        if(model.children[i].isMesh) {
+        if (model.children[i].isMesh) {
             model.children[i].material = model.children[i].material.clone();
+            house.add(model.children[i].clone());
+        } else {
+            // if it's not mesh, it will be added to scene
+            scene.add(model.children[i]);
         }
     }
-    scene.add(model);
 }, undefined, function (error) {
     console.error(error);
 });
@@ -51,36 +59,26 @@ loader.load('./home.glb', function (gltf) {
 camera.position.z = 20;
 camera.position.y = 15;
 camera.position.x = -15;
-window.scene = scene;
+
+let object = null;
 
 let mouse = new THREE.Vector2();
-let selectedObject = null;
+
 function onMouseClick(event) {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 0.5;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 0.5;
-
     raycaster.setFromCamera(mouse, camera);
-
-    if (window.model) {
-        const intersects = raycaster.intersectObjects(window.model.children);
-        if (intersects.length > 0) {
-            console.log(selectedObject)
-
-            selectedObject = intersects[0].object;
-        }
+    const intersects = raycaster.intersectObjects(house.children);
+    if (object) {
+        object.material.wireframe = false;
+    }
+    if (intersects.length > 0) {
+        object = intersects[0].object;
+        object.material.wireframe = true;
     }
 }
 
 window.addEventListener('mousemove', onMouseClick);
-
-window.addEventListener('click', (event) => {
-    console.log(selectedObject);
-    if (selectedObject) {
-        window.clicked = selectedObject; 
-        console.log(selectedObject);
-        selectedObject.material.wireframe = !selectedObject.material.wireframe;
-    }
-})
 
 function animate() {
     requestAnimationFrame(animate);
